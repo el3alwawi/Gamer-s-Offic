ea5c563b4d105a5ad1b802be80904794ea9d2aa3
el3alwawi <el3alwawi@hotmail.com>
2019-07-14 22:58:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,19 +26,16 @@ const dateFormat = require('dateformat');
 const pretty = require('pretty-ms') 
 var Canvas = require('canvas')
 
-const express= require('express'),
-path= require('path');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-const app =express();
-
-app.use(express.static('./dist/re-mi'));
-
-app.get('/*', (req,res)=>{
-res.sendFile(path.join(__dirname, '/dist/myapp/index.html'));
-});
-
-var port_number = .listen(process.env.PORT || 3000);
-app.listen(port_number);
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 
