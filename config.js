89aabd8e678f4841fafce5c117fd4a885e272cbb
el3alwawi <el3alwawi@hotmail.com>
2021-06-03 22:04:57 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    BOT_TOKEN: 'ODUwMDY3NjQyMjgyMDE2NzY4.YLkVLw.0nbg4OcsXGe8-CUj-rPBISJYQtw',    
-    YT_API_KEY: 'api ', 
-    prefix: '+',
-    devs: ['556833562549026816']
-}
